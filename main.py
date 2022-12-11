f341b5cad7be0259422b65ea2bcd16002b159a2d
aekkachai chatpanglurlurt <namput2557@gmail.com>
2022-12-11 22:59:10 +0700

    Aek test1

--- main.py
+++ main.py
@@ -358,7 +358,6 @@ def flexmessage(query):
         }
       }'''%(imgUrl,productName,desc,cont)
     return flex
-
 from linebot.models import (TextSendMessage,FlexSendMessage)
 import json
 def handle_text(inpmessage):
